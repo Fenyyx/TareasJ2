2bddf855d46196ff530bf558a4a5ce07e63f0b2c
Fenyyx <fenixg12@gmail.com>
2025-01-15 14:19:20 +0000

    Primera-parte

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-
+let hacker1 = "pepe"
+console.log(`The driver´s name is ${hacker1}`)
+let hacker2 = "Manolo"
+console.log(`The navigator´s name is ${hacker2}`)
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
 
 // Iteration 2: Conditionals
 
